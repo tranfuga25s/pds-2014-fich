249411c7747bb7c1506b1726ff4c57bc6b88eb72
Esteban <esteban.zeller@gmail.com>
2014-05-18 19:10:09 -0300

    / ‘TP5/convolucion_suma.m’

--- TP5/convolucion_suma.m
+++ TP5/convolucion_suma.m
@@ -1,9 +1,9 @@
 % Convolución por suma
 
 function [x] = convolucion_suma( X, Y ) 
-  tx = size( X );
-  ty = size( Y );
-  x = zeros( tx + ty +1 );
+  tx = size( X )
+  ty = size( Y )
+  x = zeros( tx + ty +1 )
   for i=1:tx
     for j=1:ty
        x(i+j-1) = x(i+j-1) + X(i) * Y(j);
