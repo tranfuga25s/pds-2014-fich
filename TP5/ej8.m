7f2f2fc23e16779eddc211caf478f8c72f8d4c57
Esteban <esteban.zeller@gmail.com>
2014-05-19 00:10:34 -0300

    / ‘TP5/ej8.m’

--- TP5/ej8.m
+++ TP5/ej8.m
@@ -32,7 +32,7 @@ Ds = zeros( length( freq ), length( t ) );
 figure(2);
 for i=1:length(freq)
   subplot( length( freq ), 1, i );
-  [ R, r ] = deconv( Ys(i,:), X );
+  [ R, r ] = deconv( X, Ys(i,:) );
   plot( R );
   Ds(i,:) = R;
   str = sprintf( "Senoidal recuperada de %i Hz", freq(i) );
