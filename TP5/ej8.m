46ecd9e757c708682797566aa41283f87e451c1b
Esteban <esteban.zeller@gmail.com>
2014-05-19 00:09:01 -0300

    / ‘TP5/ej8.m’ - ‘TP5/.ej8.m.kate-swp’

--- TP5/ej8.m
+++ TP5/ej8.m
@@ -1,15 +1,18 @@
 % Trabajo Practico Nº 5
 % Ejercicio 8
 
+clear all;
+close all;
+
 freq = 50:20:150;
 
 t = 0:0.001:2;
 
 Ss = sin( 2 * pi .* freq' .* t );
 
-Ys = zeros( length( t ), length( freq ) );
-
 X = load( 'pb100.txt' );
+
+Ys = zeros( length( freq ), length( t ) + length( X ) - 1 );
 
 % Parte 1
 % Filtrar las frecuencias
@@ -17,9 +20,20 @@ X = load( 'pb100.txt' );
 figure(1);
 for i=1:length(freq)
   subplot( length( freq ), 1, i );
-  Ys(
-  plot( conv( Ss(i,:), X ) );
+  Ys(i,:) = conv( Ss(i,:), X );
+  plot( Ys(i,:) );
   str = sprintf("Senoidal de %i Hz", freq(i) );
   title(str); 
 end
   
+% Parte 2
+% Obtener las señales originales
+Ds = zeros( length( freq ), length( t ) );
+figure(2);
+for i=1:length(freq)
+  subplot( length( freq ), 1, i );
+  [ R, r ] = deconv( Ys(i,:), X ); 
+  Ds(i,:) = R;
+  str = sprintf( "Senoidal recuperada de %i Hz", freq(i) );
+  title( str );
+end
