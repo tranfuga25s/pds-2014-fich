4364d31d5dc1d8d005837cc44e86f25c41fe1cd3
Esteban <esteban.zeller@gmail.com>
2014-05-18 23:40:35 -0300

    / ‘TP5/.ej8.m.kate-swp’ / ‘TP5/ej8.m’

--- TP5/ej8.m
+++ TP5/ej8.m
@@ -6,3 +6,15 @@ freq = 50:20:150;
 t = 0:0.001:2;
 
 Ss = sin( 2 * pi .* freq' .* t );
+
+X = load( 'pb100.txt' );
+
+% Parte 1
+% Filtrar las frecuencias
+
+figure(1);
+for i=1:length(freq)
+  subplot( length( freq ), 1, i );
+  plot( conv( Ss(i,:), X ) );
+end
+  
