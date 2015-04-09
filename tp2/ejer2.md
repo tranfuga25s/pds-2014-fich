4c0f545dd081dbbc695a39b1767d7b67731ae0ec
Tranfuga25s <tranfuga25s@gmail.com>
2015-04-09 17:33:37 -0300

    Agregado texto descriptivo ejercicio 2}

--- tp2/ejer2.md
+++ tp2/ejer2.md
@@ -5,3 +5,12 @@ Definimos el espacio de señales senoidales como:
 
 {S | S(t)=A*sin(2*pi*f*t+alpha) / A,alpha pertenecen a los reales R and f=cte }
 
+Entonces, segun los resultados observados en las graficas segun cada tipo se llego a las siguientes conclusiones:
+
+- Distinta amplitud, frecuencia y fase: No
+- Misma fase, distinta frecuencia y amplitud: No
+- Misma amplitud, distinta frecuencia: No
+- Misma frecuencia y fase, distinta amplitud: Si
+- Misma frecuencia y distinta amplitud y fase: Si
+- Senoidales por un escalar: Si
+
--- tp2/tp2ej2.m
+++ tp2/tp2ej2.m
@@ -9,42 +9,51 @@ disp("Senoidales genericas de distinta amplitud, frecuencia y fase");
 [t,S2] = senoidal(4,15,5,1000,0,3);
 NoDa = S1+S2;
 figure(1)
-plot(t,NoDa)
-title('senoidales genericas (distinta amplitud, frecuencia y fase)');
-disp("No");
-return;
+plot(t,NoDa);
+title('Senoidales genericas (distinta amplitud, frecuencia y fase)');
+disp("Senoidales genericas (distinta amplitud, frecuencia y fase): No");
+divisor_consola();
 
-%senoidales con misma fase, distinta frecuencia y amplitud
+% Senoidales con misma fase, distinta frecuencia y amplitud
 [t,S3] = senoidal(4,15,0,1000,0,3);
 NoDa2 = S1+S3;
 figure(2)
 plot(t,NoDa2)
 title('Senoidales con misma fase, distinta frecuencia y amplitud');
+disp('Senoidales con misma fase, distinta frecuencia y amplitud: No');
+divisor_consola();
 
-%senoidales con misma amplitud, distinta frecuencia
+% Senoidales con misma amplitud, distinta frecuencia
 [t,S4] = senoidal(4,5,0,1000,0,3);
 NoDa3 = S3+S4;
 figure(3)
 plot(t,NoDa3)
 title('senoidales con misma amplitud, distinta frecuencia');
+disp('Senoidales con misma amplitud, distinta frecuencia: No');
+divisor_consola();
 
-%senoidales con misma frecuencia y fase, distinta amplitud
+% Senoidales con misma frecuencia y fase, distinta amplitud
 [t,S5] = senoidal(6,5,0,1000,0,3);
 SiDa = S1+S5;
 figure(4)
 plot(t,SiDa)
 title('senoidales con misma frecuencia y fase, distinta amplitud');
+disp('Senoidales con misma frecuencia y fase, distinta amplitud: Si');
+divisor_consola();
 
-%senoidales con misma frecuencia y distinta amplitud y fase
+% Senoidales con misma frecuencia y distinta amplitud y fase
 [t,S6] = senoidal(1,5,pi/3,1000,0,3);
 [t,S7] = senoidal(3,5,0,1000,0,3);
 SiDa2 = S6+S7;
 figure(5)
 plot(t,SiDa2,'r*',t,S6,t,S7)
 title('senoidales con misma frecuencia y distinta amplitud y fase');
+disp('Senoidales con misma frecuencia y distinta amplitud y fase: Si');
+divisor_consola();
 
 PorEscalar = SiDa.*4;
 figure(6)
 plot(t,PorEscalar); %da bien
 title('senoidales PorEscalar');
-
+disp('Senoidales por un escalar: Si');
+divisor_consola();
