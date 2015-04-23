681c4c88ed0de35cada5dff723d9d52bbe2b5cd6
Tranfuga25s <tranfuga25s@gmail.com>
2015-04-23 18:15:41 -0300

    Modificaciones en clases

--- tp3-fft/tdf.m
+++ tp3-fft/tdf.m
@@ -4,10 +4,11 @@ function X = tdf(y)
     for k=0:N-1 %desde frecuencia cero
         suma=0;
         for ii=0:N-1
-            ss=exp(-(i*2*pi*k*ii)/N);
+            % ss=exp(-(i*2*pi*k*ii)/N);
             suma = suma + y(ii+1)*exp(-(i*2*pi*k*ii)/N); %producto interno contra N exponenciales ortogonal
         end
         X(k+1)=suma; %guardo aculumado y paso a la siguiente
+        display(X(k+1));
     end
 end
 
